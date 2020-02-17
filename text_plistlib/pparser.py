35a451cf05f0cd34c045be2e03cb350a1458fca3
Mingye Wang <arthur200126@gmail.com>
2020-02-17 12:23:12 +0800

    minor grammar

--- text_plistlib/pparser.py
+++ text_plistlib/pparser.py
@@ -85,16 +85,16 @@ class PlistParser(Parser):
         with self._choice():
             with self._option():
                 with self._group():
-
-                    def block1():
-                        self._entry_()
-                    self._closure(block1)
-                    self.name_last_node('s')
+                    self._value_()
+                    self.name_last_node('v')
                     self._check_eof()
             with self._option():
                 with self._group():
-                    self._value_()
-                    self.name_last_node('v')
+
+                    def block2():
+                        self._entry_()
+                    self._closure(block2)
+                    self.name_last_node('s')
                     self._check_eof()
             self._error('expecting one of: array base64data dict entry hexdata string typed value')
         self.ast._define(
